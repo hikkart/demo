6533c6b3227ac98b25b12e3758412416e55c120d
Karthik K <47769782+hikkart@users.noreply.github.com>
2022-03-31 09:29:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World From Karthik 456');
+    res.send('Hello World From Karthik 45678');
 });
 
 app.listen(PORT, HOST);
